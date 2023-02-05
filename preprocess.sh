6b49b01eec0d7178624f0f2ea09d65658dda9583
chriscrsmith <chriscs@uoregon.edu>
2023-02-04 22:12:08 -0800

    different n

--- preprocess.sh
+++ preprocess.sh
@@ -12,7 +12,7 @@
 
 
 
-box=Boxes86
+box=Boxes84
 trees=/home/chriscs/kernlab/Maps/$box/tree_list.txt
 targets=/home/chriscs/kernlab//Maps/$box/target_list.txt
 
@@ -28,14 +28,14 @@ targets=/home/chriscs/kernlab//Maps/$box/target_list.txt
 module load miniconda
 conda activate /home/chriscs/Software/miniconda3/envs/disperseNN
 
-n=10
-python disperseNN2/disperseNN2.py --out $box"_"preprocess --num_snps 5000 --max_epochs 1000 --validation_split 0.2 --batch_size 10 --threads 10 --min_n $n --max_n $n --mu 1e-15 --recapitate False --mutate True --phase 1 --polarize 2 --sampling_width 1 --num_samples 50 --edge_width 3 --preprocess --learning_rate 1e-4 --grid_coarseness 50 --seed XX --tree_list $trees --target_list $targets
+n=100
+python disperseNN2/disperseNN2.py --out $box"_"n$n"_"preprocess --num_snps 5000 --max_epochs 1000 --validation_split 0.2 --batch_size 10 --threads 10 --min_n $n --max_n $n --mu 1e-15 --recapitate False --mutate True --phase 1 --polarize 2 --sampling_width 1 --num_samples 50 --edge_width 3 --preprocess --learning_rate 1e-4 --grid_coarseness 50 --seed XX --tree_list $trees --target_list $targets
 
 
 
 # make lists
-#     find $box"_"preprocess | grep genos.npy | cut -d "/" -f 3- | cut -d "." -f 1 > $box"_"preprocess/master_list.txt
-#     for i in $(cat $box"_"preprocess/master_list.txt | cut -d "_" -f 3 ); do ls $box"_"preprocess/Genos/$i.genos.npy; done > $box"_"preprocess/geno_list.txt
-#     for i in $(cat $box"_"preprocess/master_list.txt | cut -d "_" -f 3 ); do ls $box"_"preprocess/Locs/$i.locs.npy; done > $box"_"preprocess/loc_list.txt
-#     for i in $(cat $box"_"preprocess/master_list.txt | cut -d "_" -f 3 ); do ls $box"_"preprocess/Maps/$i.target.npy; done > $box"_"preprocess/map_list.txt
+#     find $box"_"n$n"_"preprocess | grep genos.npy | cut -d "/" -f 3- | cut -d "." -f 1 > $box"_"n$n"_"preprocess/master_list.txt
+#     for i in $(cat $box"_"n$n"_"preprocess/master_list.txt | cut -d "_" -f 3 ); do ls $box"_"n$n"_"preprocess/Genos/$i.genos.npy; done > $box"_"n$n"_"preprocess/geno_list.txt
+#     for i in $(cat $box"_"n$n"_"preprocess/master_list.txt | cut -d "_" -f 3 ); do ls $box"_"n$n"_"preprocess/Locs/$i.locs.npy; done > $box"_"n$n"_"preprocess/loc_list.txt
+#     for i in $(cat $box"_"n$n"_"preprocess/master_list.txt | cut -d "_" -f 3 ); do ls $box"_"n$n"_"preprocess/Maps/$i.target.npy; done > $box"_"n$n"_"preprocess/map_list.txt
 
