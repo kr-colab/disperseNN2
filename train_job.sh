9f0f26e940c7a24a3b6bf2f74d8c5f6e580f8a72
chriscrsmith <chriscs@uoregon.edu>
2023-02-09 05:47:15 -0800

    small edit to train_job.sh

--- train_job.sh
+++ train_job.sh
@@ -8,8 +8,7 @@
 #SBATCH --account=kernlab       ### Account used for job submission 
 #SBATCH --mem=50gb
 #SBATCH --cpus-per-task 2
-#SBATCH --gpus=2g.10gb:1 # --gpus=2g.10gb:1 --gpus=3g.20gb:1
-
+#SBATCH --gpus=2g.10gb:1 # --gpus=2g.10gb:1 --gpus=3g.20gb:1   
 
 
 
@@ -21,19 +20,19 @@ module list
 nvidia-smi
 
 
-# TRAIN
+### TRAIN ###
 # notes:
-# - 50gb ram, and 2g.10gb:1 should work with current archtecture
-id=5
+# - 50gb ram, and 2g.10gb:1 for 45 pairs. 3g.20gb:1 for 450 pairs. (once you go bigger, you run into GPU memory limits, and then RAM limits)
+box=85
+id=1
 u=6
-n=100
-pairs=4950
-date=0207
-#python disperseNN2/disperseNN2.py --out Boxes84_preprocess/ --num_snps 5000 --max_epochs 1000 --validation_split 0.2 --batch_size 10 --threads 1 --min_n $n --max_n $n --mu 1e-15 --seed $id --recapitate False --mutate True --phase 1 --polarize 2 --sampling_width 1 --num_samples 50 --edge_width 3 --train --preprocessed --learning_rate 1e-4 --grid_coarseness 50 --upsample $u --pairs $pairs --gpu_index any > Boxes84_preprocess/pwConv.$id.txt_upsample$u"_"pairs$pairs"_"$date
-#
-python disperseNN2/disperseNN2.py --out Boxes84_n100_preprocess/ --num_snps 5000 --max_epochs 1000 --validation_split 0.2 --batch_size 10 --threads 1 --min_n $n --max_n $n --mu 1e-15 --seed $id --recapitate False --mutate True --phase 1 --polarize 2 --sampling_width 1 --num_samples 50 --edge_width 3 --train --preprocessed --learning_rate 1e-4 --grid_coarseness 50 --upsample $u --pairs $pairs --gpu_index any  > Boxes84_n100_preprocess/pwConv.$id.txt_upsample$u"_"pairs$pairs
+n=10
+pairs=45
+date=0209
+echo "python disperseNN2/disperseNN2.py --out Boxes$box"_"n$n"_"preprocess/ --num_snps 5000 --max_epochs 1000 --validation_split 0.2 --batch_size 10 --threads 1 --min_n $n --max_n $n --mu 1e-15 --seed $id --recapitate False --mutate True --phase 1 --polarize 2 --sampling_width 1 --num_samples 50 --edge_width 3 --train --preprocessed --learning_rate 1e-4 --grid_coarseness 50 --upsample $u --pairs $pairs --gpu_index any  > Boxes$box"_"n$n"_"preprocess/pwConv.$id.txt_upsample$u"_"pairs$pairs"_"$date"
+python disperseNN2/disperseNN2.py --out Boxes$box"_"n$n"_"preprocess/ --num_snps 5000 --max_epochs 1000 --validation_split 0.2 --batch_size 10 --threads 1 --min_n $n --max_n $n --mu 1e-15 --seed $id --recapitate False --mutate True --phase 1 --polarize 2 --sampling_width 1 --num_samples 50 --edge_width 3 --train --preprocessed --learning_rate 1e-4 --grid_coarseness 50 --upsample $u --pairs $pairs --gpu_index any  > Boxes$box"_"n$n"_"preprocess/pwConv.$id.txt_upsample$u"_"pairs$pairs"_"$date
 
-# # pred
+### PRED ###
 # id=1
 # v=31
 # u=4
@@ -43,17 +42,4 @@ python disperseNN2/disperseNN2.py --out Boxes84_n100_preprocess/ --num_snps 5000
                                                                                
 
 
-
-
-# # ordinal
-# id=2
-# v=40
-# u=4
-# python /home/chriscs/kernlab/Maps/Maps/Gaussian_sigma/disperseNN_v$v.py --out Boxes84/pwConv.$id.v$v --num_snps 5000 --max_epochs 1000 --validation_split 0.2 --batch_size 10 --threads 10 --min_n 10 --max_n 10 --mu 1e-15 --seed 12345 --tree_list Boxes84/tree_list.txt --target_list Boxes84/target_list_ordinal.txt --recapitate False --mutate True --phase 1 --polarize 2 --sampling_width 1 --num_samples 50 --edge_width 3 --train --learning_rate 1e-4 --grid_coarseness 50 --upsample $u --gpu_index any  > Boxes84/pwConv.$id.txt_v$v"_"upsample$u
-
-# # pred
-# id=2
-# v=40
-# u=4
-# python /home/chriscs/kernlab/Maps/Maps/Gaussian_sigma/disperseNN_v$v.py --out Boxes84/pwConv.$id.v$v --num_snps 5000 --max_epochs 1000 --validation_split 0.2 --batch_size 10 --threads 10 --min_n 10 --max_n 10 --mu 1e-15 --seed 12345 --tree_list Boxes84/tree_list.txt --target_list Boxes84/target_list_ordinal.txt --recapitate False --mutate True --phase 1 --polarize 2 --sampling_width 1 --num_samples 50 --edge_width 3 --predict --learning_rate 1e-4 --grid_coarseness 50 --upsample $u --gpu_index any --training_params Boxes84/pwConv.$id.v$v"_"training_params.npy --load_weights Boxes84/pwConv.$id.v$v"_"model.hdf5 --num_pred 1000
 
