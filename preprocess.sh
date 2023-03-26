8e5f8ad21ab210121bcc160b38c1b3dc51cdcb0a
chriscrsmith <chriscs@uoregon.edu>
2023-03-26 19:53:14 +0000

    swapped min_n for n

--- preprocess.sh
+++ preprocess.sh
@@ -46,19 +46,19 @@ conda activate /home/chriscs/Software/miniconda3/envs/disperseNN
 
 
 # regular example
-#python disperseNN2/disperseNN2.py --out $box"_"n$n"_"preprocess --num_snps 5000 --max_epochs 1000 --validation_split 0.2 --batch_size 10 --threads 10 --min_n $n --max_n $n --mu 1e-15 --recapitate False --mutate True --sampling_width 1 --num_samples 50 --edge_width 3 --preprocess --learning_rate 1e-4 --grid_coarseness 50 --seed XX --tree_list $trees --target_list $targets
+#python disperseNN2/disperseNN2.py --out $box"_"n$n"_"preprocess --num_snps 5000 --max_epochs 1000 --validation_split 0.2 --batch_size 10 --threads 10 --n $n --mu 1e-15 --recapitate False --mutate True --sampling_width 1 --num_samples 50 --edge_width 3 --preprocess --learning_rate 1e-4 --grid_coarseness 50 --seed XX --tree_list $trees --target_list $targets
 
 # grid sampling example
-#python disperseNN2/disperseNN2.py --out $box"_"n$n"_"preprocess_grid --num_snps 5000 --max_epochs 1000 --validation_split 0.2 --batch_size 10 --threads 10 --min_n $n --max_n $n --mu 1e-15 --recapitate False --mutate True --sampling_width 1 --num_samples 50 --edge_width 3 --preprocess --learning_rate 1e-4 --grid_coarseness 50 --seed XX --tree_list $trees --target_list $targets $grid
+#python disperseNN2/disperseNN2.py --out $box"_"n$n"_"preprocess_grid --num_snps 5000 --max_epochs 1000 --validation_split 0.2 --batch_size 10 --threads 10 --n $n --mu 1e-15 --recapitate False --mutate True --sampling_width 1 --num_samples 50 --edge_width 3 --preprocess --learning_rate 1e-4 --grid_coarseness 50 --seed XX --tree_list $trees --target_list $targets $grid
 
 # segment example- ordinal maps 
-#python disperseNN2/disperseNN2.py --out $box"_"preprocess/ --num_snps 5000 --max_epochs 1000 --validation_split 0.2 --batch_size 1 --threads 1 --min_n 10 --max_n 10 --mu 1e-15 --seed XX --recapitate False --mutate True --sampling_width 1 --num_samples 50 --edge_width 3 --preprocess --learning_rate 1e-4 --grid_coarseness 50 --upsample 6 --pairs 45 --segment --target_list $targets
+#python disperseNN2/disperseNN2.py --out $box"_"preprocess/ --num_snps 5000 --max_epochs 1000 --validation_split 0.2 --batch_size 1 --threads 1 --n 10 --mu 1e-15 --seed XX --recapitate False --mutate True --sampling_width 1 --num_samples 50 --edge_width 3 --preprocess --learning_rate 1e-4 --grid_coarseness 50 --upsample 6 --pairs 45 --segment --target_list $targets
 
 # 2-channel
-#python disperseNN2/disperseNN2_dev_twoChannel.py --out $box"_"n$n"_"preprocess --num_snps 5000 --max_epochs 1000 --validation_split 0.2 --batch_size 10 --threads 10 --min_n $n --max_n $n --mu 1e-15 --recapitate False --mutate True --sampling_width 1 --num_samples 50 --edge_width 3 --preprocess --learning_rate 1e-4 --grid_coarseness 50 --seed XX --tree_list $trees --target_list $targets
+#python disperseNN2/disperseNN2_dev_twoChannel.py --out $box"_"n$n"_"preprocess --num_snps 5000 --max_epochs 1000 --validation_split 0.2 --batch_size 10 --threads 10 --n $n --mu 1e-15 --recapitate False --mutate True --sampling_width 1 --num_samples 50 --edge_width 3 --preprocess --learning_rate 1e-4 --grid_coarseness 50 --seed XX --tree_list $trees --target_list $targets
 
 # one-sigma
-python disperseNN2/disperseNN2_dev_oneSigma.py --out $box"_"n$n"_"$snps"snps_"preprocess_ONESIG --num_snps $snps --max_epochs 1000 --validation_split 0.2 --batch_size 1 --threads 1 --min_n $n --max_n $n --mu 1e-15 --seed XX --recapitate False --mutate True --sampling_width 1 --num_samples 50 --edge_width 3 --learning_rate 1e-4 --tree_list $trees --target_list $targets --preprocess
+python disperseNN2/disperseNN2_dev_oneSigma.py --out $box"_"n$n"_"$snps"snps_"preprocess_ONESIG --num_snps $snps --max_epochs 1000 --validation_split 0.2 --batch_size 1 --threads 1 --n $n --mu 1e-15 --seed XX --recapitate False --mutate True --sampling_width 1 --num_samples 50 --edge_width 3 --learning_rate 1e-4 --tree_list $trees --target_list $targets --preprocess
 
 
 
