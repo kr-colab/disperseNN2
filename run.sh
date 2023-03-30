03e8ddbfb535c02b9fa23c8c7b3721f647833051
chriscrsmith <chriscs@uoregon.edu>
2023-03-30 17:57:34 +0000

    small change

--- run.sh
+++ run.sh
@@ -44,7 +44,11 @@ DATE=$(date | awk '{print $2,$3}' | sed s/" "//g)
 
                 # PRED 
 #python disperseNN2/disperseNN2.py --out Boxes$box"_"n$n"_"preprocess_ONESIG --num_snps 5000 --max_epochs 1000 --validation_split 0.2 --batch_size 1 --threads 1 --n $n --mu 1e-15 --seed $id --recapitate False --mutate True --num_samples 50 --predict --learning_rate 1e-4 --preprocessed --pairs $pairs --load_weights Boxes105_106_n23_preprocess_ONESIG/out140_boxes105_noProj_model.hdf5 --num_pred 100 --gpu_index -1
-# train 105, test 106
+#
+python disperseNN2/disperseNN2.py --out out_one_sig --num_snps 5000 --max_epochs 1000 --validation_split 0.2 --batch_size 10 --n 100 --mu 1e-15 --seed 38 --recapitate False --mutate True --learning_rate 1e-4 --preprocessed --pairs_encode 45 --pairs_downsample 45 --pairs_set 45 --predict --num_pred 1000 --load_weights out_one_sig/pwConv_38_model.hdf5 --gpu_index 1  
+#
+python disperseNN2/disperseNN2.py --out out_one_sig --num_snps 5000 --max_epochs 1000 --validation_split 0.2 --batch_size 10 --n 100 --mu 1e-15 --seed 39 --recapitate False --mutate True --learning_rate 1e-4 --preprocessed --pairs_encode 4000 --pairs_downsample 100 --pairs_set 100 --predict --num_pred 1000 --load_weights out_one_sig/pwConv_39_model.hdf5 --gpu_index -1 
+
 
 
 
