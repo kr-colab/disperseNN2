fc702989bad3abec09325a44620be0aabf524a39
chriscrsmith <chriscs@uoregon.edu>
2023-07-21 23:39:26 +0000

    moved more stuff

---
+++ disperseNN2/docs/source/conf.py
@@ -0,0 +1,46 @@
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information
+
+project = "disperseNN"
+copyright = "2023, The disperseNN authors"
+
+release = "0.1"
+version = "0.1.0"
+
+# -- General configuration
+
+extensions = [
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+    "sphinx_copybutton",
+]
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3/", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
+}
+intersphinx_disabled_domains = ["std"]
+
+templates_path = ["_templates"]
+
+# -- Options for HTML output
+
+html_theme = "sphinx_rtd_theme"
+
+# -- Options for EPUB output
+epub_show_urls = "footnote"
+
+# options for copy-button
+# (https://sphinx-copybutton.readthedocs.io/en/latest/use.html)
+copybutton_prompt_text = (
+    r">>> |\.\.\. |\$ |In \[\d*\]: | {2,5}\.\.\.: | {5,8}: |^>|\(\.venv\) \$ "
+)
+copybutton_prompt_is_regexp = True
+copybutton_only_copy_prompt_lines = True
+copybutton_remove_prompts = True
+# copybutton_copy_empty_lines = True
+# copybutton_line_continuation_character = "\\"
