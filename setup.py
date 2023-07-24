f7a306a5a9fee6bd828bc4b74f3d255eca19fcc3
chriscrsmith <chriscs@uoregon.edu>
2023-07-24 20:49:32 +0000

    pip install works with no-deps; or with pip install .

--- setup.py
+++ setup.py
@@ -1,15 +1,11 @@
 import os
 from setuptools import setup
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='disperseNN2',
-      version='4.0.5',
+      version='4.0.7',
       description='neural net for estimating dispersal',
       long_description=read('README'),
       long_description_content_type="text/markdown",
@@ -18,14 +14,16 @@ setup(name='disperseNN2',
       author_email='chriscs@uoregon.edu',
       license='MIT',
       packages=['disperseNN2',],
-      # install_requires=['numpy',
-      #                   'geopy',
-      #                   'attrs',
-      #                   'scikit-learn',
-      #                   'msprime',
-      #                   'tskit',
-      #                   'utm',
-      #                   'matplotlib'],
+      install_requires=['tensorflow==2.11.*',
+                        'gpustat',
+                        'numpy',
+                        'geopy',
+                        'attrs',
+                        'scikit-learn',
+                        'msprime',
+                        'tskit',
+                        'utm',
+                        'matplotlib'],
       scripts=['disperseNN2/disperseNN2',
                'disperseNN2/data_generation.py',
                'disperseNN2/read_input.py',
@@ -36,10 +34,4 @@ setup(name='disperseNN2',
           "Topic :: Utilities",
           "License :: OSI Approved :: BSD License",
       ],      
-      #zip_safe=False,
-      # extras_require={
-      #     'dev': [],
-      # },
-      # setup_requires=[],
-      # ext_modules=[]
 )
